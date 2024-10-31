6c25a7697913d9c185a2a6f436dcd33a2df698f7
Colin Huber <cbhuber@gmail.com>
2024-10-30 21:12:33 -0600

    fix: gh pages config

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -41,7 +41,12 @@ const Header = () => {
     >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+          <img
+            src="/xora-landing-page/images/xora.svg"
+            width={115}
+            height={55}
+            alt="logo"
+          />
         </a>
 
         <div
@@ -70,7 +75,7 @@ const Header = () => {
                     )}
                   >
                     <img
-                      src="/images/xora.svg"
+                      src="/xora-landing-page/images/xora.svg"
                       width={160}
                       height={55}
                       alt="logo"
@@ -88,14 +93,14 @@ const Header = () => {
 
             <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
               <img
-                src="/images/bg-outlines.svg"
+                src="/xora-landing-page/images/bg-outlines.svg"
                 width={960}
                 height={380}
                 alt="outline"
                 className="relative z-2"
               />
               <img
-                src="/images/bg-outlines-fill.png"
+                src="/xora-landing-page/images/bg-outlines-fill.png"
                 width={960}
                 height={380}
                 alt="outline"
@@ -110,7 +115,7 @@ const Header = () => {
           onClick={() => setIsOpen((prevState) => !prevState)}
         >
           <img
-            src={`/images/${isOpen ? "close" : "magic"}.svg`}
+            src={`/xora-landing-page/images/${isOpen ? "close" : "magic"}.svg`}
             alt="magic"
             className="size-1/2 object-contain"
           />
